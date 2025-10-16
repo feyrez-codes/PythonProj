35067c71f6170d8241eefeca1e0f56cad99a862d
Walwil8809 <colemanwallace8382@gmail.com>
2025-10-16 14:28:23 -0400

    Write app vision, play with GUI

--- config/index.py
+++ config/index.py
@@ -1,2 +1,21 @@
-print("hello world")
+###############################
+# Python GUI Project
+#
+# Notes:
+
+# If you are here from Github, Hello! 
+# To 
+
+###############################
+
+#Press Play to being interactin
+
+
+#Primary GUI superset
+import tkinter as tk
+from tkinter import messagebox
+
+
+
+root = tk.Tk()
 
