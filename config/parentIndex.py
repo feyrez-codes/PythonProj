81001ea8aa98edf6d52c4a9a13603110d76867d7
Walwil8809 <colemanwallace8382@gmail.com>
2025-10-21 08:39:15 -0400

    Writing parent app

---
+++ config/parentIndex.py
@@ -0,0 +1,23 @@
+###############################
+# Python GUI Project
+#
+# Notes:
+
+# If you are here from Github, Hello! 
+# Take a look at my ReadMe for details on the project. 
+
+# As of 10/16/25, this project is not functional. Please check back soon as I am working on this every day!
+
+###############################
+
+#Primary GUI superset
+import tkinter as tk
+from tkinter import messagebox
+
+
+
+root = tk.Tk()
+
+root.title("My First Tinkter application!")
+
+root.mainloop()
